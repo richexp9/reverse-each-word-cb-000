c5da2cae477c1dfd11662eab794bbeb2e6cd98dc
Richard Loera <richexp9@gmx.com>
2018-01-14 22:05:55 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ def reverse_each_word(sentence)
   list = ''
   sentenceArray = sentence.split(' ')
   sentenceArray.each { |e|
-    list= "#{list}+#{e.reverse}"
+    list= "#{list} #{e.reverse}"
   }
   list
   #sentence.reverse
